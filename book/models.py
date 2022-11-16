da003b66eb469c206a76f0d66c3a6a8bdee61987
tmannazy <e.anene@hotmail.com>
2022-11-16 15:49:14 +0100

    Create book model

--- book/models.py
+++ book/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Book(models.Model):
+	GENRE_CHOICES = (
+		("C", "Comedy",),
+		("T", "Tragedy"),
+		("TC", "Tragicomedy"),
+		("H", "Horror"),
+		("CR", "Crime"),
+		("R", "Romance"),
+		("SF", "Sci-Fi")
+	)
+	title = models.CharField(max_length=255)
+	slug = models.SlugField(db_index=True, default="-")
+	isbn = models.CharField(max_length=20)
+	date_published = models.DateField()
+	price = models.DecimalField(max_digits=6, decimal_places=2)
+	date_added = models.DateField(auto_now_add=True)
+	edition = models.PositiveSmallIntegerField()
+	genre = models.CharField(max_length=2, choices=GENRE_CHOICES, default="R")
