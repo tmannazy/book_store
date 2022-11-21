2b954ad50f8fb39b5ed266dd1554f596bc00446a
tmannazy <e.anene@hotmail.com>
2022-11-21 12:39:52 +0100

    Update to models and settings

--- book/models.py
+++ book/models.py
@@ -4,6 +4,12 @@ from django.core.validators import MinLengthValidator, MaxLengthValidator
 
 # Create your models here.
 
+class Publisher(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    website = models.URLField()
+
+
 class Book(models.Model):
     GENRE_CHOICES = (
         ("C", "Comedy",),
@@ -22,23 +28,21 @@ class Book(models.Model):
     date_added = models.DateField(auto_now_add=True)
     edition = models.PositiveSmallIntegerField()
     genre = models.CharField(max_length=2, choices=GENRE_CHOICES, default="R")
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE, related_name="books")
+    authors = models.ManyToManyField("Author", related_name="books")
 
 
 class Author(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    email = models.EmailField()
-
-
-class Publisher(models.Model):
-    name = models.CharField(max_length=255)
-    email = models.EmailField()
-    website = models.URLField()
+    email = models.EmailField(unique=True)
 
 
 class Address(models.Model):
     number = models.PositiveSmallIntegerField()
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
-    zipcode = models.CharField(max_length=6, validators=[MinLengthValidator(5, "code cannot be less than a length of 5"),
-                                                         MaxLengthValidator(6, "code cannotexceed length of 6")])
+    zipcode = models.CharField(max_length=6,
+                               validators=[MinLengthValidator(5, "code cannot be less than a length of 5"),
+                                           MaxLengthValidator(6, "code cannot exceed length of 6")])
+    publisher = models.OneToOneField(Publisher, on_delete=models.CASCADE)
